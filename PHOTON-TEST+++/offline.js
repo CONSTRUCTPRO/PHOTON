d1a519284c2586c1f375c083e8cbc67ee44469e8
CONSTRUCTPRO <98707822+CONSTRUCTPRO@users.noreply.github.com>
2022-01-30 19:21:36 +0100

    Add files via upload

---
+++ PHOTON-TEST+++/offline.js
@@ -0,0 +1,22 @@
+﻿{
+	"version": 1643564737,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/sprite-sheet0.png",
+		"images/peer-sheet0.png",
+		"images/snakepartmy-sheet0.png",
+		"images/snakepartmy-sheet1.png",
+		"images/snakepartothers-sheet0.png",
+		"images/snakepartothers-sheet1.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png",
+		"Photon-Javascript_SDK.min.js"
+	]
+}
